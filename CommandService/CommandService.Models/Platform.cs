7c62a7c3760b9730dc7d1e7698a40aff0e7425c8
pradeepdharmasena <dharmasenatap@gmail.com>
2023-12-20 19:27:33 +0530

    after sql db config

---
+++ CommandService/CommandService.Models/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int ExternalId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
+  
+    }
+}
